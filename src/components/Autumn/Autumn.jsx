80a748b8c3a4a61373f052716e45a91303b12251
Your Name <you@example.com>
2024-06-04 14:26:48 +0500

    end

--- src/components/Autumn/Autumn.jsx
+++ src/components/Autumn/Autumn.jsx
@@ -5,8 +5,8 @@ import Collection from '../Collection/Collection'
 import AutumnImg from "../../assets/autumn.png"
 import { DATA } from '../../lib/data'
 const Autumn = () => {
-    const {t} = useTranslation()
-    const title = t("autumn.title")
+  const title = t("autumn.title")
+  const {t} = useTranslation()
     const subtitle = t("autumn.subtitle")
     const data = DATA(t).filter(item=> item?.category === "Autumn Collection")
   return (
