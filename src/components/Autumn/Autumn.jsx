c804f4ea5c4eb58aa035dd3e877f830a721b3acc
Your Name <you@example.com>
2024-06-04 14:27:16 +0500

    ending

--- src/components/Autumn/Autumn.jsx
+++ src/components/Autumn/Autumn.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import Quality from '../Quality/Quality'
-import Collection from '../Collection/Collection'
-import AutumnImg from "../../assets/autumn.png"
-import { DATA } from '../../lib/data'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Quality from "../Quality/Quality";
+import Collection from "../Collection/Collection";
+import AutumnImg from "../../assets/autumn.png";
+import { DATA } from "../../lib/data";
 const Autumn = () => {
-  const title = t("autumn.title")
-  const {t} = useTranslation()
-    const subtitle = t("autumn.subtitle")
-    const data = DATA(t).filter(item=> item?.category === "Autumn Collection")
+  const { t } = useTranslation();
+  const title = t("autumn.title");
+  const subtitle = t("autumn.subtitle");
+  const data = DATA(t).filter((item) => item?.category === "Autumn Collection");
   return (
-    <section className=''>
-      <Quality image={AutumnImg} bg=" bg-gray-700"/>
-      <Collection title={title} subtitle={subtitle} data={data}/>
+    <section className="">
+      <Quality image={AutumnImg} bg=" bg-gray-700" />
+      <Collection title={title} subtitle={subtitle} data={data} />
     </section>
-  )
-}
+  );
+};
 
-export default Autumn
+export default Autumn;
